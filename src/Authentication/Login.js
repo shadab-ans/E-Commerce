2f4d4126da0bdedc2cbf8331c1fbdecf13446bc6
Abhishek Raj <bcoder48@gmail.com>
2022-09-19 08:26:17 +0530

    my first commit

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -8,7 +8,7 @@ import { auth } from "../firebase-config";
 
 
 function Login(props) {
-   
+
 
     // new 
     const [registerEmail, setRegisterEmail] = useState("");
@@ -26,15 +26,15 @@ function Login(props) {
     // })
     useEffect(() => {
         auth.onAuthStateChanged(
-          async (user) => {
-            if (user) {
-              setUser(user);
-            //   props.setUser(user);
-            }
-          },
-          [user]
+            async (user) => {
+                if (user) {
+                    setUser(user);
+                    //   props.setUser(user);
+                }
+            },
+            [user]
         );
-      });
+    });
     const Register = async () => {
         try {
             const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
@@ -43,7 +43,7 @@ function Login(props) {
         catch (error) {
             console.log(error.message);
         }
-    
+
     }
 
     const Login = async () => {
@@ -55,32 +55,32 @@ function Login(props) {
         catch (error) {
             console.log(error.message);
         }
-      
+
     }
 
     const Logout = async () => {
         await signOut(auth);
     }
 
-    
-    
+
+
     return (
-        
+
         <>
             <section className='LoginPage'>
                 <div className='register-css'>
                     <h3>Register New  User</h3>
-                    <input id='emailInput'   placeholder='Email...' onChange={(e) => setRegisterEmail(e.target.value)} />
-                    <input id='emailPassword'  placeholder='Password...' onChange={(e) => setRegisterPassword(e.target.value)} />
-                    <button id="loginBtn"  onClick={Register}>Create User</button>
+                    <input id='emailInput' placeholder='Email...' onChange={(e) => setRegisterEmail(e.target.value)} />
+                    <input id='emailPassword' placeholder='Password...' onChange={(e) => setRegisterPassword(e.target.value)} />
+                    <button id="loginBtn" onClick={Register}>Create User</button>
                 </div>
                 <div className='login-css'>
                     <h3>Login</h3>
                     <input id="loginInput" placeholder='Email...' onChange={(e) => setLoginEmail(e.target.value)} />
-                    <input id="loginPassword"   placeholder='Password...' onChange={(e) => setLoginPassword(e.target.value)} />
+                    <input id="loginPassword" placeholder='Password...' onChange={(e) => setLoginPassword(e.target.value)} />
                     <button id="loginBtn" onClick={Login}>Login</button>
                 </div>
-                <div style={{display:'none'}}>
+                <div style={{ display: 'none' }}>
                     <h4>User Logged In: </h4>
                     {user?.email}
                     <button id="loginBtn" onClick={Logout}>Sign out</button>
